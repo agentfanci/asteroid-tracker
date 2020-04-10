a1d0cda2f5d94aba8cd54790bb0495774017ae4e
Agent Fanci <ahflintgruber@knox.edu>
2020-04-10 15:37:07 -0500

    Add comments

--- asteroid tracker/src/AsteroidTrackerTest.java
+++ asteroid tracker/src/AsteroidTrackerTest.java
@@ -5,21 +5,21 @@ import org.junit.jupiter.api.Test;
 class AsteroidTrackerTest {
 
 	@Test
-	void addTest() {
+	void addTest() { //can you add anything at all
 		AsteroidTracker a = new AsteroidTracker();
 		a.addAsteroid("Big Rock", 40);
 	}
 	
 	@Test
-	void getNextTest() {
+	void getNextTest() { //does get next get the thing
 		AsteroidTracker a = new AsteroidTracker();
-		a.addAsteroid("The littler one", 20);
+		a.addAsteroid("The big one", 20);
 		String n = a.getNext().getName();
-		assertEquals("The littler one", n);
+		assertEquals("The big one", n);
 	}
 	
 	@Test
-	void reorderTest() {
+	void reorderTest() { //does it order by danger and not when you put it in
 		AsteroidTracker a = new AsteroidTracker();
 		a.addAsteroid("Little rock", 10);
 		a.addAsteroid("Big rock", 50);
@@ -29,7 +29,7 @@ class AsteroidTrackerTest {
 	}
 	
 	@Test
-	void endTest() {
+	void endTest() { //can you add more after the current were done, and then add another and get the all done message again?
 		AsteroidTracker a = new AsteroidTracker();
 		a.addAsteroid("Little rock", 10);
 		a.addAsteroid("Big rock", 50);
